fe222e88aba3df3f78f13f46b0c39f971b1959f3
Andrius Mozūraitis <andrius.mozuraitis@adeoweb.biz>
2018-01-11 16:19:21 +0200

    Added parram generation

--- generators/app/templates/go-kit-seed/src/dto.go
+++ generators/app/templates/go-kit-seed/src/dto.go
@@ -3,9 +3,15 @@ package main
 <% for(endpoint of endpoints) { %>
 
 // <%= endpoint.methodName %>Request Data transfer object
-type <%= endpoint.methodName %>Request struct {}
+type <%= endpoint.methodName %>Request struct {<% for(p of endpoint.parrams.request) { %>
+	<%= p.goKey %> string `json:"<%= p.jsonKey %>"`
+<% } %>
+}
 
 // <%= endpoint.methodName %>Response Data transfer object
-type <%= endpoint.methodName %>Response struct {}
+type <%= endpoint.methodName %>Response struct {<% for(p of endpoint.parrams.response) { %>
+	<%= p.goKey %> string `json:"<%= p.jsonKey %>"`
+<% } %>
+}
 
 <% } %>
